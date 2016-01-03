45a460e3e28c47091dc5f300161b3e781c33e268
Bronson Quick <bronson@sennza.com.au>
2016-01-03 16:10:14 +1000

    Add in PHPUnit 4.8.x

--- modules/tester/manifests/init.pp
+++ modules/tester/manifests/init.pp
@@ -1,7 +1,8 @@
 class tester (
 	$install_path = "/usr/local/src/phpunit",
+	$install_old_path = "/usr/local/src/phpunitold",
 ) {
-	# Create the install path
+	# Create the install paths
 	file { $install_path:
 		ensure => directory,
 	}
@@ -25,5 +26,31 @@ class tester (
 		ensure => link,
 		target => "$install_path/phpunit.phar",
 		require => File[ "$install_path/phpunit.phar" ],
+	}
+
+	# Create the old install path
+	file { $install_old_path:
+		ensure => directory,
+	}
+
+	# Download old phpunit
+	exec { "phpunitold download":
+		command => "/usr/bin/curl -o $install_old_path/phpunit-old.phar -L https://phar.phpunit.de/phpunit-old.phar",
+		require => [ Package[ 'curl' ], File[ $install_old_path ] ],
+		creates => "$install_old_path/phpunit-old.phar",
+	}
+
+	# Ensure we can run old phpunit
+	file { "$install_old_path/phpunit-old.phar":
+		ensure => "present",
+		mode => "a+x",
+		require => Exec[ 'phpunitold download' ]
+	}
+
+	# Symlink it across
+	file { '/usr/bin/phpunitold':
+		ensure => link,
+		target => "$install_old_path/phpunit-old.phar",
+		require => File[ "$install_old_path/phpunit-old.phar" ],
 	}
 }
