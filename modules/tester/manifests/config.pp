f84244af8afd254c289c6f0c9cc0aca598442414
Stephen Edgar <stephen@netweb.com.au>
2017-03-21 21:10:46 +1100

    Puppet coding standards

--- modules/tester/manifests/config.pp
+++ modules/tester/manifests/config.pp
@@ -1,9 +1,10 @@
+# Tester extension for Chassis
 class tester::config {
-	file { "/vagrant/extensions/tester/wpdevel/wp-tests-config.php":
-		content => template("tester/wp-tests-config.php.erb"),
-	}
+  file { '/vagrant/extensions/tester/wpdevel/wp-tests-config.php':
+    content => template('tester/wp-tests-config.php.erb'),
+  }
 
-	file { "/etc/profile.d/tester-env.sh":
-		content => template("tester/tester-env.sh.erb"),
-	}
+  file { '/etc/profile.d/tester-env.sh':
+    content => template('tester/tester-env.sh.erb'),
+  }
 }
