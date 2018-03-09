12c45ae2f4c3a56d8dc02ab0f61fab4984d95bd4
Bronson Quick <bronson@bronsonquick.com.au>
2018-03-09 10:51:36 +1000

    Fix linting issues

--- modules/tester/manifests/config.pp
+++ modules/tester/manifests/config.pp
@@ -1,18 +1,18 @@
 # Tester extension for Chassis
 class tester::config {
-  if ( ! empty( $config[disabled_extensions] ) and 'chassis/tester' in $config[disabled_extensions] ) {
+  if ( ! empty( $::config[disabled_extensions] ) and 'chassis/tester' in $::config[disabled_extensions] ) {
     $file = absent
   } else {
     $file = present
   }
 
   file { '/vagrant/extensions/tester/wpdevel/wp-tests-config.php':
-    content => template('tester/wp-tests-config.php.erb'),
-    ensure  => $file
+    ensure  => $file,
+    content => template('tester/wp-tests-config.php.erb')
   }
 
   file { '/etc/profile.d/tester-env.sh':
-    content => template('tester/tester-env.sh.erb'),
-    ensure  => $file
+    ensure  => $file,
+    content => template('tester/tester-env.sh.erb')
   }
 }
