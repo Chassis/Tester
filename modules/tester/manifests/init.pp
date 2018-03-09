a35f3e2ab6901fa5b2047dd82c974f6c7becf08b
Bronson Quick <bronson@bronsonquick.com.au>
2018-03-09 10:46:57 +1000

    Add disabled_extension support

--- modules/tester/manifests/init.pp
+++ modules/tester/manifests/init.pp
@@ -3,36 +3,54 @@ class tester (
   $config,
   $install_path = '/usr/local/src/phpunit',
 ) {
-  # Create the install path
-  file { $install_path:
-    ensure => directory,
-  }
 
-  if ( $config[php] < 5.6 ) {
-    $phpunit_repo_url = 'https://phar.phpunit.de/phpunit-4.8.phar'
+  if ( ! empty( $config[disabled_extensions] ) and 'chassis/tester' in $config[disabled_extensions] ) {
+    $tester = absent
   } else {
-    $phpunit_repo_url = 'https://phar.phpunit.de/phpunit-5.7.phar'
+    $tester = present
   }
 
-  # Download phpunit
-  exec { 'phpunit download':
-    command => "/usr/bin/curl -o ${install_path}/phpunit.phar -L ${phpunit_repo_url}",
-    require => [ Package[ 'curl' ], File[ $install_path ] ],
-    creates => "${install_path}/phpunit.phar",
-  }
+  if ( present == $tester ) {
+    # Create the install path
+    file { $install_path:
+      ensure => directory,
+    }
 
-  # Ensure we can run phpunit
-  file { "${install_path}/phpunit.phar":
-    ensure  => 'present',
-    mode    => 'a+x',
-    require => Exec[ 'phpunit download' ]
-  }
+    if ( $config[php] < 5.6 ) {
+      $phpunit_repo_url = 'https://phar.phpunit.de/phpunit-4.8.phar'
+    } else {
+      $phpunit_repo_url = 'https://phar.phpunit.de/phpunit-5.7.phar'
+    }
 
-  # Symlink it across
-  file { '/usr/bin/phpunit':
-    ensure  => link,
-    target  => "${install_path}/phpunit.phar",
-    require => File[ "${install_path}/phpunit.phar" ],
+    # Download phpunit
+    exec { 'phpunit download':
+      command => "/usr/bin/curl -o ${install_path}/phpunit.phar -L ${
+        phpunit_repo_url}",
+      require => [ Package[ 'curl' ], File[ $install_path ] ],
+      creates => "${install_path}/phpunit.phar",
+    }
+
+    # Ensure we can run phpunit
+    file { "${install_path}/phpunit.phar":
+      ensure  => present,
+      mode    => 'a+x',
+      require => Exec[ 'phpunit download' ]
+    }
+
+    # Symlink it across
+    file { '/usr/bin/phpunit':
+      ensure  => link,
+      target  => "${install_path}/phpunit.phar",
+      require => File[ "${install_path}/phpunit.phar" ],
+    }
+  } else {
+    file { $install_path:
+      ensure => $tester,
+      force  => true
+    }
+    file { '/usr/bin/phpunit':
+      ensure  => absent
+    }
   }
 
   class { 'tester::config': }
