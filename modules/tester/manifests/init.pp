d2e9bb5ac42289673a5ac501604f1a913580ff25
Bronson Quick <bronson@bronsonquick.com.au>
2018-07-16 14:54:11 +1000

    Wrap the version in quotes

--- modules/tester/manifests/init.pp
+++ modules/tester/manifests/init.pp
@@ -17,7 +17,7 @@ class tester (
 			ensure => directory,
 		}
 
-		if ( $config[php] < 5.6 ) {
+		if ( $config[php] < '5.6' ) {
 			$phpunit_repo_url = 'https://phar.phpunit.de/phpunit-4.8.phar'
 		} else {
 			$phpunit_repo_url = 'https://phar.phpunit.de/phpunit-6.5.phar'
