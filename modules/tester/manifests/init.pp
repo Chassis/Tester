efce6a730569af8f073138709714eac8ce1c813b
Bronson Quick <bronson@bronsonquick.com.au>
2019-03-07 19:14:37 +1000

    Add phpunit as a dependency

--- modules/tester/manifests/init.pp
+++ modules/tester/manifests/init.pp
@@ -1,7 +1,6 @@
 # Tester extension for Chassis
 class tester (
 	$config,
-	$install_path = '/usr/local/src/phpunit',
 ) {
 
 	if ( !empty($config[disabled_extensions]) and 'chassis/tester' in $config[
@@ -12,38 +11,6 @@ class tester (
 	}
 
 	if ( present == $tester ) {
-		# Create the install path
-		file { $install_path:
-			ensure => directory,
-		}
-
-		if versioncmp( $config[php], '5.6' ) == 0 {
-			$phpunit_repo_url = 'https://phar.phpunit.de/phpunit-4.8.phar'
-		} else {
-			$phpunit_repo_url = 'https://phar.phpunit.de/phpunit-5.7.phar'
-		}
-
-		# Download phpunit
-		exec { 'phpunit download':
-			command => "/usr/bin/curl -o ${install_path}/phpunit.phar -L ${
-				phpunit_repo_url}",
-			require => [ Package[ 'curl' ], File[ $install_path ] ],
-			creates => "${install_path}/phpunit.phar",
-		}
-
-		# Ensure we can run phpunit
-		file { "${install_path}/phpunit.phar":
-			ensure  => present,
-			mode    => 'a+x',
-			require => Exec[ 'phpunit download' ]
-		}
-
-		# Symlink it across
-		file { '/usr/bin/phpunit':
-			ensure  => link,
-			target  => "${install_path}/phpunit.phar",
-			require => File[ "${install_path}/phpunit.phar" ],
-		}
 
 		if ( $config[tester_db] ) {
 			mysql::db { $config[tester_db][name]:
@@ -55,14 +22,5 @@ class tester (
 		}
 
 		class { 'tester::config': }
-
-		} else {
-			file { $install_path:
-				ensure => $tester,
-				force  => true
-			}
-			file { '/usr/bin/phpunit':
-				ensure => absent
-		}
 	}
 }
