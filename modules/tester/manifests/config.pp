a3076119ad030504aae1f5c8018d3ee0a91540e6
K Adam White <kadamwhite@users.noreply.github.com>
2020-07-06 17:50:44 -0400

    Add support for generating local-config-tester.php file

--- modules/tester/manifests/config.pp
+++ modules/tester/manifests/config.pp
@@ -1,5 +1,11 @@
 # Tester extension for Chassis
-class tester::config {
+class tester::config (
+	$test_database = undef,
+	$test_database_user = undef,
+	$test_database_password = undef,
+	$test_database_host = undef,
+	$test_database_prefix = 'test_',
+) {
   if ( ! empty( $::config[disabled_extensions] ) and 'chassis/tester' in $::config[disabled_extensions] ) {
     $file = absent
   } else {
@@ -16,9 +22,18 @@ class tester::config {
     content => template('tester/tester-env.sh.erb')
   }
 
-  exec { 'load_tester_envs':
-    path      => '/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin',
-    command   => "bash -c 'source /etc/profile.d/tester-env.sh'",
-    subscribe => File['/etc/profile.d/tester-env.sh']
+  if ( $file == absent or $test_database ) {
+    file { '/vagrant/local-config-tester.php':
+      ensure  => $file,
+      content => template('tester/local-config-tester.php.erb')
+    }
+  }
+
+  if ( present == $file ) {
+    exec { 'load_tester_envs':
+      path      => '/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin',
+      command   => "bash -c 'source /etc/profile.d/tester-env.sh'",
+      subscribe => File['/etc/profile.d/tester-env.sh']
+    }
   }
 }
--- modules/tester/manifests/init.pp
+++ modules/tester/manifests/init.pp
@@ -20,13 +20,19 @@ class tester (
 				grant    => ['all']
 			}
 		}
-
-		class { 'tester::config': }
 	} else {
 		exec { 'unset env variables':
 			command  => 'unset WP_DEVELOP_DIR; unset WP_TESTS_DIR;',
 			path     => '/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin',
 			provider => 'shell'
 		}
+	}
+
+	# Invoke even if tester is disabled in order to clean up templated files.
+	class { 'tester::config':
+		test_database => $config[tester_db][name],
+		test_database_user => $config[tester_db][user],
+		test_database_password => $config[tester_db][password],
+		test_database_host => $config[tester_db][host],
 	}
 }
